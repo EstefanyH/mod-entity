a6a69f66baba36cc7ffdbde747af8f36ff0060e3
EstefanyH <estefanyhuamani287@gmail.com>
2024-04-28 14:14:56 -0500

    add struct user

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Entity-Module",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Entity-Module",
+            targets: ["Entity-Module"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Entity-Module"),
+        .testTarget(
+            name: "Entity-ModuleTests",
+            dependencies: ["Entity-Module"]),
+    ]
+)
---
+++ Sources/Entity-Module/User.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Valeria on 28/04/24.
+//
+
+import Foundation
+
+public struct User {
+    var Id: String
+    var Name: String
+    var Profile: String
+}
